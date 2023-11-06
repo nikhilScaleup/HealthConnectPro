2bce06353520e46edd4ce9774ebda1f7265ddfe5
nikhilScaleup <64263508+nikhilScaleup@users.noreply.github.com>
2023-11-06 17:55:32 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -64,7 +64,7 @@ if($_POST){
             <tr>
                 <td colspan="2">
 				
-                    <p class="header-text">Let's Get Started</p>
+                    <p class="header-text">Let's Get Started the test case</p>
 					
                     <p class="sub-text">Add Your Personal Details to Continue</p>
                 </td>
