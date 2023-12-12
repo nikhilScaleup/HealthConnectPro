c5c2178decd9a7b57e4eaf86a7ce6230e3f3b822
nikhilScaleup <64263508+nikhilScaleup@users.noreply.github.com>
2023-12-11 22:19:24 -0500

    Update login.php

--- login.php
+++ login.php
@@ -140,7 +140,7 @@
 						<?php
 						  }
 				       ?>
-                    <p class="sub-text" style="color:#F77D24">Login</p>
+                    <p class="sub-text" style="color:#F77D24">Login Test Login</p>
                 </td>
             </tr>
             <tr>
